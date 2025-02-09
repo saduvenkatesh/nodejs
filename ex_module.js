7b1c861f4a070113d48ffa301d7b6e061d74c2d1
SADU VENKATESH <saduvenkatesh141205@gmail.com>
2025-02-09 19:10:12 +0530

    passing data as a object through modules

--- ex_module.js
+++ ex_module.js
@@ -1,7 +1,11 @@
 //const names = require('./names')
-const {name1, name2} = require('./names') // Another way (destructing object)
+//const {name1, name2} = require('./names') // Another way (destructing object)
 const sayHi = require('./greetFunc')
+const data = require('./names')
+console.log(data);
+console.log(data.venkatesh.name,data.venkatesh.ra_no, data.venkatesh.cgpa);
+console.log(data.reethu.name,data.reethu.ra_no, data.reethu.cgpa);
 
-sayHi(name1)
-sayHi(name2)
+//sayHi(name1)
+//sayHi(name2)
 sayHi('Sadu')
--- names.js
+++ names.js
@@ -1,5 +1,18 @@
 // Names
-const name1 = "Venkatesh"
-const name2 = "Ramu"
 
-module.exports = {name1, name2}
+module.exports.students = ['Venkatesh', 'Reethu']
+
+
+const venkatesh = {
+    name : "Venkatesh",
+    ra_no : "RA2311003011042",
+    cgpa : 9.09 
+}
+
+const reethu = {
+    name : "Reethu",
+    ra_no : "RA23110030110420",
+    cgpa  : 10
+}
+module.exports.venkatesh = venkatesh
+module.exports.reethu = reethu
